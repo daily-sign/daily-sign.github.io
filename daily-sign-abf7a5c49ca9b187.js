8bf61b30dc4b76c1f5aa6078662bc9bfab4b7d50
OmmyZhang <OmmyZhang@users.noreply.github.com>
2025-08-28 17:27:54 +0000

    update name and dep f309b447889e98cd7f546233363f9ae1122193bb

--- daily-sign-abf7a5c49ca9b187.js
+++ daily-sign-abf7a5c49ca9b187.js
@@ -722,7 +722,7 @@ async function __wbg_init(module_or_path) {
     }
 
     if (typeof module_or_path === 'undefined') {
-        module_or_path = new URL('easy-sign_bg.wasm', import.meta.url);
+        module_or_path = new URL('daily-sign_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
