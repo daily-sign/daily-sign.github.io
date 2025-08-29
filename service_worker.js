978cbed8638425f83f00df6dc08d9ab01c273148
OmmyZhang <OmmyZhang@users.noreply.github.com>
2025-08-29 16:41:38 +0000

    nice PWA ced46cdda89c94b8781cd0b9dc9181991780ee83

--- service_worker.js
+++ service_worker.js
@@ -1,27 +1,49 @@
-var cacheName = 'yew-pwa';
-var filesToCache = [
-  './',
-  './daily-sign.js',
-  './daily-sign.wasm',
-  '//gcore.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js',
-  '//gcore.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css',
-];
+// Catch every request
+// Modified from https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Guides/Offline_and_background_operation
 
+const putInCache = async (request, response) => {
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
+};
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
+const cacheFirst = async (request) => {
+  // First try to get the resource from the cache.
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    // still update cache for next time
+    fetch(request)
+      .then(putInCache.bind(null, request))
+      .catch((error) => {
+        console.error("Background update failed:", error, request.url);
+      });
+    return responseFromCache;
+  }
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+  // If the response was not found in the cache,
+  // try to get the resource from the network.
+  try {
+    const responseFromNetwork = await fetch(request);
+    // If the network request succeeded, clone the response:
+    // - put one copy in the cache, for the next time
+    // - return the original to the app
+    // Cloning is needed because a response can only be consumed once.
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    console.error(
+      "Fetching failed:",
+      error,
+      request.url,
+      "Returning offline page instead."
+    );
+    return new Response("Network error happened", {
+      status: 408,
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
+};
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    cacheFirst(event.request));
 });
