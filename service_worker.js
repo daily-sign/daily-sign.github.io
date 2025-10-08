416addc749bd89329fc21cc2dbf8fa999edda783
OmmyZhang <OmmyZhang@users.noreply.github.com>
2025-10-08 17:32:29 +0000

    sw cache strategy 843582d00a1c49288f5bc1556adfb21f9821b56c

--- service_worker.js
+++ service_worker.js
@@ -1,10 +1,18 @@
-// Catch every request
 // Modified from https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Guides/Offline_and_background_operation
+import { idbKeyval as storage } from './js_utils/storage.js';
+
 
 const putInCache = async (request, response) => {
   const cache = await caches.open("v1");
   await cache.put(request, response);
 };
+
+let dbInstance;
+
+const failbackResponse = new Response("Network error happened", {
+  status: 408,
+  headers: { "Content-Type": "text/plain" },
+});
 
 const cacheFirst = async (request) => {
   // First try to get the resource from the cache.
@@ -36,14 +44,40 @@ const cacheFirst = async (request) => {
       request.url,
       "Returning offline page instead."
     );
-    return new Response("Network error happened", {
-      status: 408,
-      headers: { "Content-Type": "text/plain" },
-    });
+    return failbackResponse;
+  }
+};
+
+const cacheByTime = async (request, expire) => {
+  // store cache time in indexedDB
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    const ts = await storage.get(request.url);
+    if (ts && (Date.now() - ts < expire * 1000)) {
+      return responseFromCache;
+    }
+  }
+  try {
+    const responseFromNetwork = await fetch(request);
+    await storage.set(request.url, Date.now());
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    console.error("Fetching failed:", error);
+    return responseFromCache || failbackResponse;
   }
 };
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    cacheFirst(event.request));
+  const request = event.request;
+
+  if (request.url.startsWith(self.location.origin)) {
+    // Use cache strategy for same-origin assets
+    event.respondWith(cacheFirst(request));
+  } else if (request.url.startsWith("https://hitscounter.dev/api/hit")) {
+    event.respondWith(cacheByTime(request, 300));
+  } else if (request.url.startsWith("https://gcore.jsdelivr.net/")) {
+    // cache CDN resources
+    event.respondWith(cacheByTime(request, 3600 * 24));
+  }
 });
