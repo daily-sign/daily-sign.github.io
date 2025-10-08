1baf9b42c46ce0a7f828639aa5e79d71f6e1a8b2
OmmyZhang <OmmyZhang@users.noreply.github.com>
2025-10-08 18:46:10 +0000

    move storage.js & fix caches.match 20cb3602942743799f942c0f9439b63afd843189

--- service_worker.js
+++ service_worker.js
@@ -1,9 +1,68 @@
-// Modified from https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Guides/Offline_and_background_operation
-import { idbKeyval as storage } from './js_utils/storage.js';
+// https://github.com/jakearchibald/svgomg/blob/main/src/js/utils/storage.js
+const storage = (() => {
+  let dbInstance;
 
+  function getDB() {
+    if (dbInstance) return dbInstance;
+
+    dbInstance = new Promise((resolve, reject) => {
+      const openreq = indexedDB.open('my-keyval', 2);
+
+      openreq.onerror = () => {
+        reject(openreq.error);
+      };
+
+      openreq.onupgradeneeded = () => {
+        // First time setup: create an empty object store
+        openreq.result.createObjectStore('keyval');
+      };
+
+      openreq.onsuccess = () => {
+        resolve(openreq.result);
+      };
+    });
+
+    return dbInstance;
+  }
+
+  async function withStore(type, callback) {
+    const db = await getDB();
+    return new Promise((resolve, reject) => {
+      const transaction = db.transaction('keyval', type);
+      transaction.oncomplete = () => resolve();
+      transaction.onerror = () => reject(transaction.error);
+      callback(transaction.objectStore('keyval'));
+    });
+  }
+
+  return {
+    async get(key) {
+      let request;
+      await withStore('readonly', (store) => {
+        request = store.get(key);
+      });
+      return request.result;
+    },
+    set(key, value) {
+      return withStore('readwrite', (store) => {
+        store.put(value, key);
+      });
+    },
+    delete(key) {
+      return withStore('readwrite', (store) => {
+        store.delete(key);
+      });
+    },
+  };
+})();
+
+
+// Modified from https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Guides/Offline_and_background_operation
+
+const CACHE_NAME = "v2";
 
 const putInCache = async (request, response) => {
-  const cache = await caches.open("v2");
+  const cache = await caches.open(CACHE_NAME);
   await cache.put(request, response);
 };
 
@@ -16,7 +75,7 @@ const failbackResponse = new Response("Network error happened", {
 
 const cacheFirst = async (request) => {
   // First try to get the resource from the cache.
-  const responseFromCache = await caches.match(request);
+  const responseFromCache = await caches.open(CACHE_NAME).then((cache) => cache.match(request));
   if (responseFromCache) {
     // still update cache for next time
     fetch(request)
@@ -50,7 +109,7 @@ const cacheFirst = async (request) => {
 
 const cacheByTime = async (request, expire) => {
   // store cache time in indexedDB
-  const responseFromCache = await caches.match(request);
+  const responseFromCache = await caches.open(CACHE_NAME).then((cache) => cache.match(request));;
   if (responseFromCache) {
     const ts = await storage.get(request.url);
     if (ts && (Date.now() - ts < expire * 1000)) {
